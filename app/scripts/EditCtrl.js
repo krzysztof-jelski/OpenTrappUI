c0975c3f7538454109be2e2e85600c8191f9c551
Michael Bareja <michal.bareja@pragmatists.pl>
2014-05-09 11:52:31 +0200

    #3 edit a wotkload (misses validation)

--- app/scripts/EditCtrl.js
+++ app/scripts/EditCtrl.js
@@ -1,24 +1,69 @@
 angular.module('openTrapp')
-	.controller('EditCtrl', function ($scope, $modal) {
+    .controller('EditCtrl', function ($scope, $modal, projectNames, $http) {
 
-  $scope.open = function () {
+        $scope.open = function (item) {
 
-    var modalInstance = $modal.open({
-      templateUrl: 'edit.html',
-      controller: EditModalCtrl,
-      resolve: {}
+            var modalInstance = $modal.open({
+                templateUrl: 'edit.html',
+                controller: EditModalCtrl,
+                resolve: {
+                    item: function () {
+                        return item;
+                    },
+                    projectNames: function () {
+                        return projectNames;
+                    },
+                    http: function () {
+                        return $http;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                // FIXME - publish alert to some global alert displayer
+                $scope.alerts = $scope.alerts || [];
+                $scope.alerts.push(result)
+            })
+
+        };
     });
 
-  };
-});
+var EditModalCtrl = function ($scope, $modalInstance, item, projectNames, http) {
 
-var EditModalCtrl = function ($scope, $modalInstance) {
+    var suggestions = [];
+    (function gatherSuggestions() {
+        suggestions = [];
+        projectNames.forEach(function (name) {
+            suggestions.push(name);
+        })
+    })();
+    $scope.item = angular.copy(item);
 
-  $scope.ok = function () {
-    $modalInstance.close();
-  };
+    function getSuggestions(prefix) {
+        return _.filter(suggestions, function (suggestion) {
+            return suggestion.indexOf(prefix) != -1;
+        });
+    }
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+    $scope.$watch('item.projectName', function (newValue) {
+        $scope.suggestions = getSuggestions(newValue);
+    });
+
+    $scope.ok = function () {
+        var data = {
+            workload: $scope.item.workload,
+            projectName: $scope.item.projectName
+        };
+        http.post('http://localhost:8080/endpoints/v1/work-log/entries/' + item.id, data)
+            .success(function () {
+                $modalInstance.close({
+                    type: 'success',
+                    message: 'Worklog updated'
+                });
+            });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 };
