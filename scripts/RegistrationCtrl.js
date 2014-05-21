9c1beb1aa12d13720532d34b03bbf89c24685493
michal.piotrkowski <michal.piotrkowski@pragmatists.pl>
2014-05-21 16:05:39 +0200

    Publish to github pages from grunt

--- scripts/RegistrationCtrl.js
+++ scripts/RegistrationCtrl.js
@@ -1,77 +1,50 @@
 angular.module('openTrapp').controller('RegistrationCtrl',
-		function($scope, $http, currentEmployee, worklogEntryParser, projectNames, $sce) {
+    function ($scope, $http, currentEmployee, worklogEntryParser, $sce) {
 
-			$scope.alerts = [];
-			$scope.workLogExpression = '';
-			$scope.suggestions = [];
-			$scope.clearAlerts = function(){
-				$scope.alerts = [];
-			};
-			$scope.logWork = function(){
-				
-				if(!worklogEntryParser.isValid($scope.workLogExpression)){
-					return;
-				}
-				var data = worklogEntryParser.parse($scope.workLogExpression);
+        $scope.alerts = [];
+        $scope.workLogExpression = '';
+        $scope.suggestions = [];
+        $scope.clearAlerts = function () {
+            $scope.alerts = [];
+        };
+        $scope.logWork = function () {
 
-				$http
-					.post('http://localhost:8080/endpoints/v1/employee/' + currentEmployee.username() + '/work-log/entries', data)
-					.success(function(response, status){
-						$scope.workLogExpression = '';
-						update();
-                        var message = sprintf("<b>Hurray!</b> You  have successfully logged <b>%s</b> on project <b>%s</b> at <b>%s</b>.", data.workload, data.projectName, data.day);
-                        $scope.alerts = [{ type: 'success', message: $sce.trustAsHtml(message)}];
-					}).error(function(response,status){
-                        var message = '<b>Upps...</b> Server is not responding.';
-                        $scope.alerts = [{ type: 'danger', message: $sce.trustAsHtml(message)}];
-                    });
-			};
+            if (!worklogEntryParser.isValid($scope.workLogExpression)) {
+                return;
+            }
+            var data = worklogEntryParser.parse($scope.workLogExpression);
 
-			var r = /.*#([^\s]*)$/;
-			var editingProjectName = function(input){
-				return r.exec(input)[1];
-			};
-			var isEditingProjectName = function(input){
-				return r.test(input)
-			};
-			var calculateSuggestions = function(input){
-				if (isEditingProjectName(input)){
-					var prefix = editingProjectName(input);
-					$scope.suggestions = [];
-					projectNames.startingWith(prefix).forEach(function(x){
-						$scope.suggestions.push(x);
-					});
-				} else{
-					$scope.suggestions = [];
-				}
-			};
-			
-			var update = function(){
-				
-				if($scope.workLogExpression == ''){
-					$scope.status = '';
-					return;
-				}
-				
-				if(worklogEntryParser.isValid($scope.workLogExpression)){
-					$scope.status = 'success';
-				} else{
-					$scope.status = 'error';
-				}
-			};
-			
-			// AngularUI sucks
-			var tmp = '';
-			
-			$scope.$watch('workLogExpression', function(newVal, oldVal){
-				tmp = newVal;
-				calculateSuggestions(newVal);
-				update();
-			});
-			
-			$scope.selectSuggestion = function(suggestion){
-				
-				var prefix = editingProjectName(tmp);
-				$scope.workLogExpression = tmp.replace('#' + prefix, '#' + suggestion + ' '); 
-			};
-		});
+            $http
+                .post('http://localhost:8080/endpoints/v1/employee/' + currentEmployee.username() + '/work-log/entries', data)
+                .success(function (response, status) {
+                    $scope.workLogExpression = '';
+                    update();
+                    var message = sprintf("<b>Hurray!</b> You  have successfully logged <b>%s</b> on project <b>%s</b> at <b>%s</b>.", data.workload, data.projectName, data.day);
+                    $scope.alerts = [
+                        { type: 'success', message: $sce.trustAsHtml(message)}
+                    ];
+                }).error(function (response, status) {
+                    var message = '<b>Upps...</b> Server is not responding.';
+                    $scope.alerts = [
+                        { type: 'danger', message: $sce.trustAsHtml(message)}
+                    ];
+                });
+        };
+
+        var update = function () {
+
+            if ($scope.workLogExpression == '') {
+                $scope.status = '';
+                return;
+            }
+
+            if (worklogEntryParser.isValid($scope.workLogExpression)) {
+                $scope.status = 'success';
+            } else {
+                $scope.status = 'error';
+            }
+        };
+
+        $scope.$watch('workLogExpression', update);
+
+    });
