1ae3f4ebd3a5736a2d82e25fa77be1e3e4cd0fa4
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-12-14 14:32:23 +0100

    Publish to github pages from grunt

--- scripts/WorklogExpressionDirective.js
+++ scripts/WorklogExpressionDirective.js
@@ -2,12 +2,13 @@ angular.module('openTrapp')
     .directive('worklogExpression', function ($compile, projectNames, datesSuggestions) {
         return {
             restrict: 'E',
+            replace: 'true',
             templateUrl: 'templates/worklog-expression-template.html',
 
             link: function ($scope, element) {
-                inputElement = $(element).children()[0];
+                var inputElement = element[0];
 
-                var lastDesiredEffectOfCompletion = new EffectOfCompletion(0);
+                var effectOfLastCompletion = new EffectOfCompletion(0);
 
                 var suggestionSourceFor = {
                     '#': projectNames,
@@ -36,7 +37,7 @@ angular.module('openTrapp')
                     var completion = tag.symbol + suggestion + ' ';
                     var match = replacementRegexp.exec(currentExpression);
                     $scope.workLogExpression = currentExpression.replace(replacementRegexp, completion);
-                    lastDesiredEffectOfCompletion = new EffectOfCompletion(match.index + completion.length);
+                    effectOfLastCompletion = new EffectOfCompletion(match.index + completion.length);
                 };
 
                 $scope.getCursorPosition = function () {
@@ -44,8 +45,8 @@ angular.module('openTrapp')
                 };
 
                 $scope.$watch('workLogExpression', function (newVal, oldVal) {
-                    if (lastDesiredEffectOfCompletion.isNotApplied()) {
-                        lastDesiredEffectOfCompletion.apply();
+                    if (effectOfLastCompletion.isNotApplied()) {
+                        effectOfLastCompletion.apply();
                     }
                 });
 
